3428bc3bb892cf91e180b56f4564fa586abebc5a
Kieran <kieran.webdevelopment@gmail.com>
2022-07-17 08:16:29 +0100

    created useFetch hook. fetched data from json server

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,4 +1,19 @@
+import { useFetch } from '../../hooks/useFetch.js';
+
 import './home.css';
 export default function Home() {
-  return <div>home</div>;
+  const {
+    data: recipes,
+    isPending,
+    error,
+  } = useFetch(`http://localhost:3000/recipes`);
+
+  return (
+    <div className="home">
+      {error && <p className="error">Sorry, could not get that data</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {recipes &&
+        recipes.map((recipe) => <h2 key={recipe.id}>{recipe.title}</h2>)}
+    </div>
+  );
 }
