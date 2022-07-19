14080aee427e7bd8d80d08430e072b2f51e857e0
Kieran <kieran.webdevelopment@gmail.com>
2022-07-19 14:48:58 +0100

    functionality for retrieiving individial recipes

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,17 +1,40 @@
 import { useParams } from 'react-router-dom';
-import { useFetch } from '../../hooks/useFetch';
+// import { useFetch } from '../../hooks/useFetch';
 import { useTheme } from '../../hooks/useTheme';
+import { useEffect, useState } from 'react';
+import { projectFireStore } from '../../assets/firebase/config';
 
 //styles
 import './recipe.css';
 
 export default function Recipe() {
+  // const url = `http://localhost:3000/recipes/${id}`;
+  // const { data: recipe, isPending, error } = useFetch(url);
   const { id } = useParams();
-  const url = `http://localhost:3000/recipes/${id}`;
-
-  const { data: recipe, isPending, error } = useFetch(url);
 
   const { mode } = useTheme();
+
+  const [recipe, setRecipe] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setIsPending(true);
+    projectFireStore
+      .collection('recipes')
+      .doc(id)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          setIsPending(false);
+          setRecipe(doc.data());
+        } else {
+          setIsPending(false);
+          setError('could not find that recipe.');
+        }
+      });
+  }, [id]);
+
   return (
     <div className={`recipe ${mode}`}>
       {isPending && <p className="loading">loading...</p>}
