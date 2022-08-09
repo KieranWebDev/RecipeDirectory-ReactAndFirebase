d712c5c5291f40acee7f16983d2dbce1a9e47781
Kieran <kieran.webdevelopment@gmail.com>
2022-08-09 07:46:58 +0100

    added delete recipe functionality and also fixed firebase back-end

--- src/components/RecipeList/RecipeList.js
+++ src/components/RecipeList/RecipeList.js
@@ -1,6 +1,9 @@
 import './recipeList.css';
 import { Link } from 'react-router-dom';
 import { useTheme } from '../../hooks/useTheme';
+import { projectFireStore } from '../../assets/firebase/config';
+
+import Trashcan from '../../assets/trashcan.svg';
 
 export default function RecipeList({ recipes }) {
   const { mode } = useTheme();
@@ -8,6 +11,11 @@ export default function RecipeList({ recipes }) {
   if (recipes.length === 0) {
     return <div className="error">No recipes to load...</div>;
   }
+
+  const handleClick = (id) => {
+    projectFireStore.collection('recipes').doc(id).delete();
+  };
+
   return (
     <div className="recipe-list">
       {recipes.map((recipe) => (
@@ -16,6 +24,12 @@ export default function RecipeList({ recipes }) {
           <p>{recipe.cookingTime} to make </p>
           <div>{recipe.tagLine}</div>
           <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
+          <img
+            className="delete"
+            src={Trashcan}
+            alt="delete-item"
+            onClick={() => handleClick(recipe.id)}
+          ></img>
         </div>
       ))}
     </div>
